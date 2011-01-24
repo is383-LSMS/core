c4f1f54090ba2504da2419815ee8da65e17f41df
Fred William <fred.haule@gmail.com>
2011-01-24 19:11:41 +0000

    initial commit i decided to make a fresh push of this new code after i fixed some major bugs

---
+++ protected/views/site/pages/about.php
@@ -0,0 +1,27 @@
+<?php
+$this->pageTitle=Yii::app()->name . ' - About';
+$this->breadcrumbs=array(
+	'About',
+);
+?>
+
+
+
+<p>LSMS is a system that is used to monitor use and access to the laboratory. it provides easy access to the schedule of the lab to all users and monitors who can book to use laboratory for free time.<br/>
+    
+    This is a project of third year University of Dar-es-Salaam students doing Internet application programming.  </p>
+<p align ="center">
+    <b> Group members includes:</b><br />
+    
+    Swai, Ruth N E <br />
+    William, Fred <br />
+    Madilu, Daniel N<br />
+    Mgimba, Christopher<br />
+    Kisegenya, Ritagati <br />
+    Minani, Clavery P <br />
+    Cyprian, Denis <br />
+  
+
+</p>
+<p>    Under supervision of <b>Ismail Kololeni</b> and <b>Mndeme Mathew.</b></p>
+<p>Codes available at <a href="www.github.com">github</a> </p>
